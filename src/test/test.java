80c8d2aa7f0a5f9b4ac3c9864ebdaaa130ca0aed
iniga72 <49905453+iniga72@users.noreply.github.com>
2021-01-17 16:17:56 +0300

    Initial commit

---
+++ src/test/test.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class test extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        Bukkit.broadcastMessage("123");
+        this.getLogger().warning("kfgvfvihffi");
+    }
+}
