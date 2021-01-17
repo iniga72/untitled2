14479c50fb0ac33cd8aec0e4903c44aaa70dac9b
iniga72 <49905453+iniga72@users.noreply.github.com>
2021-01-17 16:20:20 +0300

    a

--- src/test/test.java
+++ src/test/test.java
@@ -6,6 +6,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class test extends JavaPlugin {
     @Override
     public void onEnable() {
+        Bukkit.broadcastMessage("123");
         this.getLogger().warning("kfgvfvihffi");
     }
 }
